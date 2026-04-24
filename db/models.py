7bcf55ac74bf95a70979b76605ebb20c3226b01e
Sergio Suárez <62405866+sergio200086@users.noreply.github.com>
2026-04-23 23:07:10 -0500

    Added model to respond the questions

---
+++ db/models.py
@@ -0,0 +1,51 @@
+import chromadb
+import uuid
+from google import genai
+import os
+from dotenv import load_dotenv, find_dotenv
+
+
+load_dotenv(find_dotenv())
+
+client_genai = genai.Client(api_key=os.environ.get("GEMINI_API_KEY"))
+
+client = chromadb.PersistentClient(path="./my_models")
+collection = client.get_or_create_collection(
+    name="documents"
+)
+
+def insert_document(text, id):
+    collection.add(
+        documents=[text],
+        ids=[id]
+    )
+
+def query_document(text, results=1):
+    results = collection.query(
+        query_texts=[text],
+        n_results=2
+    )
+    return results["documents"][0]
+
+
+def answer_question(text):
+    results = query_document(text)
+    context = "\n".join(results)
+
+    response = client_genai.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=f"Basándote en estos documentos:\n\n{context}\n\nResponde: {text}"
+    )
+
+    return response.text
+
+
+# insert_document("Invoice 001 from Acme Inc for $5000 for consulting services", str(uuid.uuid4()))
+# insert_document("Lease agreement valid for 12 months starting March 1st", str(uuid.uuid4()))
+# insert_document("Your payment of $3200 has been confirmed", str(uuid.uuid4()))
+# answer_question("my car Radiator doesn't work") 
+
+
+print(answer_question("¿Cuánto facturó Acme Inc?"))
+print(answer_question("¿Cuál es la duración del contrato?"))
+print(answer_question("¿Fue confirmado el pago?"))
